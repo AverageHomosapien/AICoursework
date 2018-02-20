482e7be2e1791d7899f177f9b4bbdaced1195fa7
hamcam98 <hamcalum10@gmail.com>
2018-02-20 22:09:49 +0000

    Breaking Down Data
    Input is now put into 2d array

--- AI Application/BusinessLayer/Read.cs
+++ AI Application/BusinessLayer/Read.cs
@@ -14,18 +14,36 @@ namespace BusinessLayer
         {
             string input = File.ReadAllText(filepath);
 
-
+            // Getting cave number from beginning of string
             int caveNum = Int32.Parse(input[0].ToString());
+            // Removing cave number and comma
+            input = input.Remove(0, 2);
 
+            string[,] caveCoords = new string[2,caveNum];
+
+            for (int i = 0; i < caveNum; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (input[1] == ',')
+                    {
+                        caveCoords[j,i] += input[0].ToString();
+                        input = input.Remove(0, 2);
+                    }
+                    else
+                    {
+                        caveCoords[j,i] += input[0].ToString() + input[1].ToString();
+                        input = input.Remove(0, 3);
+                    }
+                }
+
+                MessageBox.Show("Cave coordinates " + i + " are " + caveCoords[0,i]);
+                MessageBox.Show("Cave coordinates " + i + " are " + caveCoords[1, i]);
+            }
+
+            // Replacing commas below the cave coordinates are checked and removed
             input = input.Replace(",", "");
             input = input.Remove(0,1);
-
-            string[] caveCoords = new string[caveNum];
-
-            for (int i = 0; i < (caveNum * 2); i++)
-            {
-                
-            }
 
             MessageBox.Show("CaveNum = " + caveNum);
             MessageBox.Show("Input is " + input);
